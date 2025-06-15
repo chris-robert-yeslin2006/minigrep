50142a26f78f93a5110be3070ed8dd1fd135f542
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-06-15 18:48:23 +0530

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
-
+use std::fs;
 fn main() {
-    //read from command line of query and filename
+    //read from file
     let args:Vec<String>=env::args().collect();
 
     let query: &String=&args[1];
     let filename:&String=&args[2];
     println!("{:?}",query);
     println!("{:?}",filename);
+
+    let contents =fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("{}",contents);
     
 }
