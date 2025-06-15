ceaa3321c4b6470bf76f4c206ace86e576b8c1f4
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-06-15 18:41:12 +0530

    read query adn file name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
+    //read from command line of query and filename
     let args:Vec<String>=env::args().collect();
-    println!("{:?}",args);
+
+    let query: &String=&args[1];
+    let filename:&String=&args[2];
+    println!("{:?}",query);
+    println!("{:?}",filename);
+    
 }
