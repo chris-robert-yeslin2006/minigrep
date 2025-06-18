25fe9129f59acf779b79ad3c563baed8f3846268
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-06-18 14:40:24 +0530

    added implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 use std::env;
 use std::fs;
 fn main() {
-    //read from file
+    //refactor read from file
     let args:Vec<String>=env::args().collect();
 
-    let query: &String=&args[1];
-    let filename:&String=&args[2];
-    println!("{:?}",query);
-    println!("{:?}",filename);
+    let config=Config::new(&args);
+    println!("{:?}",config.query);
+    println!("{:?}",config.filename);
 
-    let contents =fs::read_to_string(filename)
+    let contents =fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("{}",contents);
     
 }
+
+struct Config{
+    query:String,
+    filename:String
+}
+impl Config{
+    fn new(args:&[String])-> Config {
+        if args.len()<3{
+            panic!("please provide query and filename");
+        }
+    let query:String=args[1].clone();
+    let filename:String=args[2].clone();
+    Config{query,filename}
+}
+}
+
+
+
